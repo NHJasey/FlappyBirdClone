6bcfdf6c13c487240da91914bf4865583454995f
Jasey <nhjasey@gmail.com>
2017-04-05 20:24:07 -0400

    Create Renderable.java

---
+++ Renderable.java
@@ -0,0 +1,13 @@
+package flappybird;
+
+import java.awt.Graphics2D;
+
+/**
+ *
+ * @author Jasey
+ */
+public interface Renderable {
+    
+    public void render(Graphics2D g, float interpolation);
+    
+}
